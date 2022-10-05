289fc2b394896b270461551bd929702b21453ce7
Ossai Lucky <luckyblaqy@gmail.com>
2022-10-05 17:01:44 +0100

    create room end points(create,update,delete,get,getall)

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,7 +1,20 @@
 import express from "express"
+import { createRoom, deleteRoom, getAllRoom, getRoom, updateRoom } from "../controllers/roomController.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
+
 
 const router = express.Router();
 
+//CREATE
+router.post("/:hotelid", verifyAdmin, createRoom);
 
+//DELETE
+router.delete("/:id",verifyAdmin, deleteRoom)
+//UPDATE
+router.put("/:id",verifyAdmin, updateRoom)
+//GET
+router.get("/:id", getRoom)
+//GET ALL
+router.get("/", getAllRoom)
 
 export default router
