23dc94ffaf065ee694962785d46dd86a9b5f68fb
Ossai Lucky <luckyblaqy@gmail.com>
2022-10-06 15:41:00 +0100

    create a city count query endpoint

--- routes/hotels.js
+++ routes/hotels.js
@@ -1,6 +1,6 @@
 import express from "express"
 import Hotel from "../models/Hotel.js";
-import { createHotel,updateHotel, deleteHotel, getAllHotel,getHotel } from "../controllers/hotelController.js";
+import { createHotel,updateHotel, deleteHotel, getAllHotel,getHotel, countByCity } from "../controllers/hotelController.js";
 import { createError } from "../utils/error.js";
 import { verifyAdmin } from "../utils/verifyToken.js";
 
@@ -14,9 +14,11 @@ router.delete("/:id",verifyAdmin, deleteHotel)
 //UPDATE
 router.put("/:id",verifyAdmin, updateHotel)
 //GET
-router.get("/:id", getHotel)
+router.get("/find/:id", getHotel)
 //GET ALL
 router.get("/", getAllHotel)
+router.get("/countByCity", countByCity)
+//router.get("/countByType", getAllHotel)
 
 
 export default router
